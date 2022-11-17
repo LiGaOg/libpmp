9186411ae200d926c0bc874d92bfd825d92ad4e9
LiGaOg <chlijiaao@163.com>
2022-11-17 10:44:28 +0800

    add printf, page alloc and free

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,12 @@
-
+#include "os.h"
 extern void uart_init(void);
-extern void uart_puts(char *s);
+extern void page_init(void);
 
 void start_kernel(void)
 {
 	uart_init();
 	uart_puts("Hello, RVOS!\n");
+	page_init();
 
 	while (1) {}; // stop here!
 }
